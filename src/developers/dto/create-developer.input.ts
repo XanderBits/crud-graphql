661ac89503ec76edbbe23bd2c501e6163dee33dd
XanderBits <amarettosyntax@protonmail.com>
2023-01-17 22:04:24 -0400

    [ADD] Developer Input (DTO)

--- src/developers/dto/create-developer.input.ts
+++ src/developers/dto/create-developer.input.ts
@@ -1,7 +1,21 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
-
+import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsString, MinLength} from 'class-validator';
 @InputType()
 export class CreateDeveloperInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  @IsString()
+  @MinLength(2)
+  name: string
+
+  @Field()
+  @IsEmail()
+  email: string
+
+  @Field(type => [Number])
+  @IsNotEmpty()
+  projects: number[]
+
+  @Field(type => [Number])
+  @IsNotEmpty()
+  roles: number[]
 }
