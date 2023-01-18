1409dd149754adcc05a1cbaf1ac5bdf0ed65de66
XanderBits <amarettosyntax@protonmail.com>
2023-01-18 19:28:23 -0400

    [ADD] Import and Exports

--- src/developers/developers.module.ts
+++ src/developers/developers.module.ts
@@ -3,13 +3,11 @@ import { DevelopersService } from './developers.service';
 import { DevelopersResolver } from './developers.resolver';
 import { Developer } from './entities/developer.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Project } from 'src/projects/entities/project.entity';
 import { Role } from 'src/roles/entities/role.entity';
 import { RolesModule } from 'src/roles/roles.module';
-import { ProjectsModule } from 'src/projects/projects.module';
-
 @Module({
   providers: [DevelopersResolver, DevelopersService],
-  imports: [TypeOrmModule.forFeature([Developer, Project, Role, RolesModule, ProjectsModule])]
+  imports: [TypeOrmModule.forFeature([Developer,Role, RolesModule,])],
+  exports: [TypeOrmModule]
 })
 export class DevelopersModule {}
--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -5,9 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
 import { RolesModule } from 'src/roles/roles.module';
 import { Role } from 'src/roles/entities/role.entity';
+import { Developer } from 'src/developers/entities/developer.entity';
+import { DevelopersModule } from 'src/developers/developers.module';
 @Module({
   providers: [ProjectsResolver, ProjectsService],
-  imports: [TypeOrmModule.forFeature([Role, Project, RolesModule])],
+  imports: [TypeOrmModule.forFeature([Role, Project, RolesModule, Developer, DevelopersModule])],
   exports: [TypeOrmModule]
 })
 export class ProjectsModule {}
