9c023b5819f261a3a9eadc84e1ce5ea994068106
XanderBits <amarettosyntax@protonmail.com>
2023-01-18 00:00:23 -0400

    [CHANGE] Method name

--- src/developers/developers.resolver.ts
+++ src/developers/developers.resolver.ts
@@ -19,6 +19,6 @@ export class DevelopersResolver {
 
   @Query(() => Developer, { name: 'developer' })
   findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.developersService.findOne(id);
+    return this.developersService.findByRoleAndProyect(id);
   }
 }
