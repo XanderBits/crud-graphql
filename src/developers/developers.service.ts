ce23a96d13c08a57bd8da237c87bbbed76e285a5
XanderBits <amarettosyntax@protonmail.com>
2023-01-18 19:30:37 -0400

    [ADD] Create Developer Service

--- src/developers/developers.service.ts
+++ src/developers/developers.service.ts
@@ -1,10 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, Injectable} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Project } from 'src/projects/entities/project.entity';
 import { Role } from 'src/roles/entities/role.entity';
 import { In, Repository } from 'typeorm';
 import { CreateDeveloperInput } from './dto/create-developer.input';
 import { Developer } from './entities/developer.entity';
+
 
 @Injectable()
 export class DevelopersService {
@@ -13,24 +13,21 @@ export class DevelopersService {
     private readonly DevRepository: Repository<Developer>,
     @InjectRepository(Role)
     private readonly RoleRepository: Repository<Role>,
-    @InjectRepository(Project)
-    private readonly ProjectRepository: Repository<Project>,
   ) {}
 
   async create(CreateDeveloperInput: CreateDeveloperInput) {
     try{
-      let { projects = [], roles = [], ...createDeveloperInput} = CreateDeveloperInput;
+      let {roles = [], ...createDeveloperInput} = CreateDeveloperInput;
+      const emailExists = await this.DevRepository.findOneBy({email: createDeveloperInput.email});
+      if(emailExists !== null) throw new HttpException(`Email address ${createDeveloperInput.email} already exists`, HttpStatus.BAD_REQUEST);
       const developer = this.DevRepository.create(createDeveloperInput);
-
       const findRoleIds = await this.RoleRepository.findBy({ id: In(roles) });
+
       developer.roles = findRoleIds;
-      const findProjectIds = await this.ProjectRepository.findBy({ id: In(projects) });
-      developer.projects = findProjectIds;
-      
-      await this.DevRepository.save( developer );
+      await this.DevRepository.save(developer);
       return developer;
     }catch(error){
-      throw new BadRequestException(error.detail)
+      return error
     } 
   }
 
