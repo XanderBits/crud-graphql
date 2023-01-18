707e96a39feecb64c9438af309d50b15437f304c
XanderBits <amarettosyntax@protonmail.com>
2023-01-18 00:02:45 -0400

    [ADD] create developer Method

--- src/developers/developers.service.ts
+++ src/developers/developers.service.ts
@@ -1,17 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from 'src/projects/entities/project.entity';
+import { Role } from 'src/roles/entities/role.entity';
+import { In, Repository } from 'typeorm';
 import { CreateDeveloperInput } from './dto/create-developer.input';
+import { Developer } from './entities/developer.entity';
 
 @Injectable()
 export class DevelopersService {
-  create(createDeveloperInput: CreateDeveloperInput) {
-    return 'This action adds a new developer';
+  constructor(
+    @InjectRepository(Developer)
+    private readonly DevRepository: Repository<Developer>,
+    @InjectRepository(Role)
+    private readonly RoleRepository: Repository<Role>,
+    @InjectRepository(Project)
+    private readonly ProjectRepository: Repository<Project>,
+  ) {}
+
+  async create(CreateDeveloperInput: CreateDeveloperInput) {
+    try{
+      let { projects = [], roles = [], ...createDeveloperInput} = CreateDeveloperInput;
+      const developer = this.DevRepository.create(createDeveloperInput);
+
+      const findRoleIds = await this.RoleRepository.findBy({ id: In(roles) });
+      console.log(findRoleIds)
+      developer.roles = findRoleIds;
+      const findProjectIds = await this.ProjectRepository.findBy({ id: In(projects) });
+      console.log(findProjectIds)
+      developer.projects = findProjectIds;
+      
+      await this.DevRepository.save( developer );
+      return developer;
+    }catch(error){
+      throw new BadRequestException(error.detail)
+    } 
   }
 
   findAll() {
     return `This action returns all developers`;
   }
 
-  findOne(id: number) {
+  findByRoleAndProyect(id: number) {
     return `This action returns a #${id} developer`;
   }
 }
